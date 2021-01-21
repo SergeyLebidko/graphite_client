eb6ff122d2678925f9f6d0adc2fbdd0d8a7fee7a
SergeyLebidko <it.sergeyler@mail.ru>
2021-01-21 18:04:08 +0300

    Добавлен компонент Preloader

--- src/App.js
+++ src/App.js
@@ -1,10 +1,21 @@
 import React from 'react';
 import style from './App.module.css';
+import Preloader from "./Preloader/Preloader";
 import Header from "./Header/Header";
 import RegisterForm from "./RegisterForm/RegisterForm";
 
 class App extends React.Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            hasInit: false
+        }
+    }
+
     render() {
+        if (!this.state.hasInit) {
+            return <Preloader/>
+        }
         return (
             [
                 <Header/>,
---
+++ src/Preloader/Preloader.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import style from './Preloader.module.css';
+
+function Preloader() {
+    let pulsars = [];
+    for (let delay = 0; delay <= 800; delay += 100) {
+        pulsars.push(<div className={style.pulsar} style={{animationDelay: `${delay}ms`}}></div>);
+    }
+    return (
+        <div className={style.preloader}>
+            {pulsars}
+        </div>
+    )
+}
+
+export default Preloader
