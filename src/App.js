427cf90cfca0560fd8e14fef019bdbd280ae4a3a
SergeyLebidko <it.sergeyler@mail.ru>
2021-01-21 18:46:18 +0300

    Добавлен запрос данных при старте приложения и файл с настройками settings.js

--- src/App.js
+++ src/App.js
@@ -1,15 +1,30 @@
 import React from 'react';
+import $ from 'jquery';
 import style from './App.module.css';
+import * as CONST from './settings';
 import Preloader from "./Preloader/Preloader";
 import Header from "./Header/Header";
 import RegisterForm from "./RegisterForm/RegisterForm";
+
 
 class App extends React.Component {
     constructor(props) {
         super(props);
         this.state = {
-            hasInit: false
+            hasInit: false,
         }
+    }
+
+    componentDidMount() {
+        $.ajax(CONST.GENDER_LIST_URL).then(data => {
+            console.log(data);
+            setTimeout(() => {
+                this.setState({
+                    hasInit: true,
+                    genderList: data
+                })
+            }, 1500);
+        });
     }
 
     render() {
---
+++ src/settings.js
@@ -0,0 +1,3 @@
+const HOST = 'http://127.0.0.1:8000'
+
+export const GENDER_LIST_URL = `${HOST}/api/gender_list/`;
