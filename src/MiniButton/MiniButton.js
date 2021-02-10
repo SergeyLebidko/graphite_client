bd7532512b6989e4acf27347eaacdb8d3c8bf0b3
SergeyLebidko <it.sergeyler@mail.ru>
2021-02-10 11:00:09 +0300

    Рефакторинг MiniButton

--- src/MiniButton/MiniButton.js
+++ src/MiniButton/MiniButton.js
@@ -2,17 +2,8 @@ import React from 'react'
 import style from './MiniButton.module.css'
 
 export const MiniButton = props => {
-    let content;
     let filePath = `/images/${props.buttonType}.svg`;
-    switch (props.buttonType) {
-        case 'ok': {
-            content = <img className={style.ok_mini_button} src={filePath} onClick={props.clickHandler}/>;
-            break;
-        }
-        case 'cancel': {
-            content = <img className={style.cancel_mini_button} src={filePath} onClick={props.clickHandler}/>;
-            break;
-        }
-    }
-    return <div className={style.mini_button_container}>{content}</div>;
+    let content = <img src={filePath} onClick={props.clickHandler}/>;
+    let targetClasses = `${style.mini_button_container} ${style[props.buttonType]}`;
+    return <div className={targetClasses}>{content}</div>;
 }
