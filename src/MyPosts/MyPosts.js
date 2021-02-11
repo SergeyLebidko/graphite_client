206ecda63d2d03ed383ae1e55db2b96b428511c9
SergeyLebidko <it.sergeyler@mail.ru>
2021-02-11 16:23:59 +0300

    Рефакторинг MyPosts.js

--- src/MyPosts/MyPosts.js
+++ src/MyPosts/MyPosts.js
@@ -14,37 +14,33 @@ class MyPosts extends React.Component {
         super(props);
         this.state = {
             hasLoad: false,
-            posts: null
+            posts: [],
+            nextPage: null
         }
     }
 
-    componentWillMount() {
+    downloadPosts() {
         let {account} = this.props;
         let token = localStorage.getItem('token');
         $.ajax(POST_URL, {
             headers: {'Authorization': token},
             data: {account: account.id}
         }).then(data => {
-            setTimeout(() => this.setState({
+            setTimeout(() => this.setState(prevState => ({
                 hasLoad: true,
-                posts: data.results
-            }), 2500);
+                posts: [...prevState.posts, ...data.results],
+                nextPage: data.next
+            })), 2500);
         })
     }
 
-    getContent() {
-        let {hasLoad, posts} = this.state;
-        if (!hasLoad) return <div className={style.preloader_block}><Preloader modal={false}/></div>;
-
-        return (
-            <>
-                {posts.map((post, index) => <PostCard key={index} post={post}/>)}
-            </>
-        )
+    componentWillMount() {
+        this.downloadPosts();
     }
 
     render() {
         let {account} = this.props;
+        let {hasLoad, posts} = this.state;
         return (
             <div className={style.my_posts_container}>
                 <div className={style.header_block}>
@@ -52,7 +48,8 @@ class MyPosts extends React.Component {
                     <AccountStat account={account}/>
                 </div>
                 <div className={style.posts_block}>
-                    {this.getContent()}
+                    {posts.map((post, index) => <PostCard key={index} post={post}/>)}
+                    {hasLoad ? '' : <div className={style.preloader_block}><Preloader modal={false}/></div>}
                 </div>
             </div>
         )
